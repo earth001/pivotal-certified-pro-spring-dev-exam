6e638fd479e9323fb2029e607b29fc8dcf6949b8
Juan Moreno <earth001@gmail.com>
2019-01-10 10:59:54 -0300

    Added solution to TODO 5, 6 and 7

--- 02-ps-container-01-practice/src/test/java/com/ps/beans/others/MtBeanTest.java
+++ 02-ps-container-01-practice/src/test/java/com/ps/beans/others/MtBeanTest.java
@@ -6,6 +6,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -20,9 +23,14 @@ public class MtBeanTest {
         //TODO 6. Modify this class to use the new set of configuration files, created by resolving TODO 5.
         //TODO 7. Try to use wildcards as well.
         ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring/others/sample-config-01.xml");
+        ApplicationContext ctxSplitted = new ClassPathXmlApplicationContext(
+            "classpath:spring/others/sample-config-01-splitted.xml");
+        ApplicationContext ctxWildcard = new ClassPathXmlApplicationContext("classpath:spring/others/split-config/*-config-01.xml");
 
         MultipleTypesBean mtBean = (MultipleTypesBean) ctx.getBean("mtBean");
         assertNotNull(mtBean);
 
+        assertThat(ctx.getBeanDefinitionCount(),is(equalTo(ctxSplitted.getBeanDefinitionCount())));
+        assertThat(ctx.getBeanDefinitionCount(),is(equalTo(ctxWildcard.getBeanDefinitionCount())));
     }
 }
