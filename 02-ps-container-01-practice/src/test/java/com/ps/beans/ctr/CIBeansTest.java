25a5ff9e7159ec7c4b4c5c4f76fd830805574ba3
Juan M.vi <earth001@gmail.com>
2019-01-09 23:57:10 -0300

    TEST Added solution to TODO 3

--- 02-ps-container-01-practice/src/test/java/com/ps/beans/ctr/CIBeansTest.java
+++ 02-ps-container-01-practice/src/test/java/com/ps/beans/ctr/CIBeansTest.java
@@ -1,13 +1,15 @@
 package com.ps.beans.ctr;
 
+import com.ps.beans.SimpleBean;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by iuliana.cosmina on 3/26/16.
@@ -21,10 +23,26 @@ public class CIBeansTest {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring/ctr/sample-config-02.xml");
 
         logger.info(" --- All beans in context --- ");
-        for(String beanName :  ctx.getBeanDefinitionNames()) {
-            logger.info("Bean " + beanName + " of type " + ctx.getBean(beanName).getClass().getSimpleName());
+        for (String beanName : ctx.getBeanDefinitionNames()) {
+            logger.info("Bean " + beanName + " of type " + ctx
+              .getBean(beanName)
+              .getClass()
+              .getSimpleName());
         }
 
         //TODO 3. Retrieve beans of types ComplexBean and make sure their dependencies were correctly set.
+        SimpleBean simpleBean0 = (SimpleBean) ctx.getBean("simpleBean0");
+        SimpleBean simpleBean1 = (SimpleBean) ctx.getBean("simpleBean1");
+
+        ComplexBeanImpl complexBean = (ComplexBeanImpl) ctx.getBean("complexBean0");
+        assertThat(complexBean.getSimpleBean(), is(equalTo(simpleBean0)));
+
+        ComplexBeanImpl complexBean1 = (ComplexBeanImpl) ctx.getBean("complexBean1");
+        assertThat(complexBean1.getSimpleBean(), is(equalTo(simpleBean0)));
+        assertThat(complexBean1.isComplex(), is(true));
+
+        ComplexBean2Impl complexBean2 = (ComplexBean2Impl) ctx.getBean("complexBean2");
+        assertThat(complexBean2.getSimpleBean1(), is(equalTo(simpleBean0)));
+        assertThat(complexBean2.getSimpleBean2(), is(equalTo(simpleBean1)));
     }
 }
